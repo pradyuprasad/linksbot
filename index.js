4b41dab280bc0cb3530d1652a1f000c6416d2be5
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2023-12-05 16:27:17 +0800

    created tables

--- index.js
+++ index.js
@@ -83,3 +83,18 @@ async function clearTable(tableName){
     console.log(res)
 }
 
+async function create_table_links(){
+    const res = await client.execute("CREATE TABLE links (link_id TEXT PRIMARY KEY, link_url TEXT, link_title TEXT, telegram_id INTEGER REFERENCES users(telegram_id), timestamp INTEGER);")
+    console.log(res)
+}
+
+async function create_table_tags(){
+    const res = await client.execute("CREATE TABLE tags (tag_id TEXT PRIMARY KEY, tag_name TEXT UNIQUE);")
+    console.log(res)
+}
+
+async function create_table_linktags(){
+    const res = await client.execute("CREATE TABLE link_tags (link_id TEXT, tag_id TEXT, PRIMARY KEY(link_id, tag_id), FOREIGN KEY (link_id) REFERENCES links(link_id), FOREIGN KEY (tag_id) REFERENCES tags(tag_id));")
+    console.log(res)
+}
+
