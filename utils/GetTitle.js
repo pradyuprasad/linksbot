54d77c8dc068e69e3616e454767b7f5e23aecefa
pradyuprasad <pradyu.sp@gmail.com>
2023-12-19 13:16:10 +0800

    fixed get title

--- utils/GetTitle.js
+++ utils/GetTitle.js
@@ -1,8 +1,10 @@
-/*import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 //import checkUrl from './UrlChecker.js'
 import { JSDOM } from 'jsdom';
 
 async function get_title(url) { 
+
+    try {
 
     let response = await fetch(url)
 
@@ -12,19 +14,67 @@ async function get_title(url) {
 
     const title = dom.window.document.querySelector('title').textContent;
     
-    return title;
+    return title;}
+
+    catch(e) {
+
+       //console.log(e)
+
+    }
+
+    try {
+
+        let modifiedURL = "https://"+url
+
+        let response = await fetch(modifiedURL)
+
+        const text = await response.text()
+
+        const dom = new JSDOM(text);
+
+        const title = dom.window.document.querySelector('title').textContent;
+        
+        return title;
+
+    }
+
+    catch(e) {
+
+        //console.log(e)
+
+    }
+
+    try{
+
+        let modifiedURL = "http://"+url
+
+        let response = await fetch(modifiedURL)
+
+        const text = await response.text()
+
+        const dom = new JSDOM(text);
+
+        const title = dom.window.document.querySelector('title').textContent;
+        
+        return title;
+
+    }
+
+    catch(e){
+        //console.log(e)
+    }
 }
 
 
 
 async function TitleGet(url) {
-    //const valid 
+    
     const result = await get_title(url)
     return result
 }
 
-const url = 'www.google.com'
+const url = 'www.goole.com'
 
 console.log(await TitleGet(url))
 
-export default TitleGet*/
+export default TitleGet
