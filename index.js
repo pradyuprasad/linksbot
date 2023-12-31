6322f29afa4c30b5e42dd810cad252718268b99b
pradyuprasad <pradyu.sp@gmail.com>
2023-12-31 15:19:21 +0530

    installed express

--- index.js
+++ index.js
@@ -5,25 +5,24 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 import { createClient } from "@libsql/client/web"; 
 import SaveText from './utils/SaveText.js';
 import CreateUser from './utils/CreateUser.js';
+import http from "http"
 
 const client = createClient({
     url: "libsql://links-pradyuprasad.turso.io",
     authToken: process.env.TURSO_TOKEN 
 })
 
-async function drop_table(table){
-    const res = await client.execute(`drop table ${table};`)
-   // console.log((res)
-}
-
-
-async function clearTable(tableName){ 
-    let res = await client.execute(`DELETE FROM ${tableName}`)
-   // console.log((res)
-}
-
 bot.start((ctx) => CreateUser(ctx, client))
 bot.on('text', (ctx) => SaveText(ctx, client))
 bot.launch()
 
 
+
+const server = http.createServer((req, res) => { 
+    // Sending the response 
+    res.write("This is the response from the server") 
+    res.end(); 
+}) 
+
+
+server.listen(8080, () => console.log("Server is working!"))
