6e7977bbe362d5bc0b6c93b840ac91aaf35ee991
pradyuprasad <pradyu.sp@gmail.com>
2024-01-07 16:45:04 +0530

    table is working

--- index.js
+++ index.js
@@ -40,18 +40,65 @@ app.get('/get_links/:url_id', async (req, res) => {
     const urlId = req.params.url_id;
     try {
         const result = await client.execute({
-            sql: "SELECT link_url, link_title FROM links JOIN users ON links.telegram_id = users.telegram_id WHERE users.url_id = ?",
+            sql: "SELECT link_url, link_title, link_id FROM links JOIN users ON links.telegram_id = users.telegram_id WHERE users.url_id = ?",
             args: [urlId]
         });
 
-        console.log(result)
-
-        res.json(result);
+        //console.log(result)
+	res.json(result);
     } catch (error) {
         res.status(500).send('Server Error');
     }
 });
 
+app.get("/delete_link/:link_id", async (req, res) => {
+	console.log("web delete is called!")
+	const link_id = req.params.link_id
+	try {
+		const result = await client.execute({
+			sql: "DELETE FROM link_tags WHERE link_id = ?",
+			args: [link_id]
+		})
+		console.log("link has been deleted!")
+
+		res.send(true)
+	}
+
+	catch(e){
+		console.log(e)
+		res.status(500).send('Server Error!')
+	}
+	try {
+		const result = await client.execute({
+				sql: "DELETE FROM links WHERE link_id = ?",
+				args: [link_id]
+			})
+		}
+
+	catch(e){
+		console.log(e)
+		res.status(500).send('Server Error!')
+	}
+})
+
+app.get("/get_tags/:link_id", async (req, res) => {
+	console.log("get tags is called")
+	const link_id = req.params.link_id
+	try {
+		const result = await client.execute({
+			sql: "SELECT tag_name FROM tags join link_tags ON tags.tag_id = link_tags.tag_id WHERE link_tags.link_id = ?",
+			args: [link_id]
+		})
+
+		console.log(result)
+		res.json(result)
+	}
+
+	catch(e){
+		console.log(e)
+		res.status(500).send('Sever Error!')
+	}
+})
 
 app.listen(port, () => {
     console.log("Express is working")
